c2821d4869d38afe4749e241e1b22d0edfcf68cf
Chris Southwick <csouthwick@live.com>
2020-08-29 23:17:52 -0400

    added quiz data

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var quizData = [{
+  q: "Commonly used data types DO NOT include:",
+  a: 2,
+  choices: ["strings", "booleans", "alerts", "numbers"]
+}, {
+  q: "The condition in an if / else statement is enclosed with _______.",
+  a: 2,
+  choices: ["quotes", "curly brackets", "parenthesis", "square brackets"]
+}, {
+  q: "Arrays in JavaScript can be used to store _______.",
+  a: 3,
+  choices: ["numbers and strings", "other arrays", "booleans", "all of the above"]
+}, {
+  q: "String values must be enclosed within _______ when being assigned to variables.",
+  a: 2,
+  choices: ["commas", "curly brackets", "quotes", "parenthesis"]
+}, {
+  q: "A very useful tool used during development and debugging for printing content to the debugger is:",
+  a: 3,
+  choices: ["JavaScript", "terminal / bash", "for loops", "console.log"]
+}];
